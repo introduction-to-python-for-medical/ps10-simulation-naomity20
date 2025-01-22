6e298b7dfd884ce3571dc19ecc53d95c15e0afb4
naomity20 <naomity20@gmail.com>
2025-01-23 00:02:02 +0200

    Update spread_fire.py

--- spread_fire.py
+++ spread_fire.py
@@ -1,11 +1,9 @@
 import copy
-
 def spread_fire(grid):
     update_grid = copy.deepcopy(grid)
     for i in range(grid_size):
         for j in range(grid_size):
             if grid[i][j] == 1:
-                # Get neighbors safely, considering boundaries
                 neighbors = []
                 if i > 0:
                     neighbors.append(grid[i-1][j]) 
@@ -15,8 +13,6 @@ def spread_fire(grid):
                     neighbors.append(grid[i][j-1]) 
                 if j < grid_size - 1:
                     neighbors.append(grid[i][j+1])  
-
                 if 2 in neighbors:
                     update_grid[i][j] = 2
-
     return update_grid
